4e9149bda4fcb2f65c2398588015a673443bf458
vjgarciag96 <vjgarciag96@gmail.com>
2017-11-29 23:42:33 +0100

    Implemented response to unknown commands and JobQueue sample

--- main.py
+++ main.py
@@ -24,6 +24,13 @@ def inline_caps(bot, update):
     )
     bot.answer_inline_query(update.inline_query.id, results)
 
+def unknown(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Lo siento, no entiendo ese comando :(')
+
+def callback_minute(bot, job):
+    bot.send_message(chat_id='187395179', text='En un minuto vuelvo')
+
+
 import telegram
 from telegram.ext import Updater, CommandHandler
 from telegram.ext import MessageHandler, Filters
@@ -38,6 +45,7 @@ print(t_bot.get_me())
 updater = Updater(token=tokens.EXTREPYTHON_BOT_TOKEN)
 updater.start_polling()
 dispatcher = updater.dispatcher
+job_queue = updater.job_queue
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
@@ -45,8 +53,12 @@ start_command_handler = CommandHandler('start', start)
 echo_handler = MessageHandler(Filters.text, echo)
 caps_handler = CommandHandler('caps', caps, pass_args=True)
 inline_caps_handler = InlineQueryHandler(inline_caps)
+unknown_handler = MessageHandler(Filters.command, unknown)
+
+job_minute = job_queue.run_repeating(callback_minute, interval=60, first=0)
 
 dispatcher.add_handler(start_command_handler)
 dispatcher.add_handler(echo_handler)
 dispatcher.add_handler(caps_handler)
 dispatcher.add_handler(inline_caps_handler)
+dispatcher.add_handler(unknown_handler)
